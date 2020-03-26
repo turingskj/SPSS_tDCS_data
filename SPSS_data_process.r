a26e9da3694f6a8c73dcb92b0c5b7e297af60698
turingskj <turingskj@hotmail.com>
2020-03-26 02:59:39 -0400

    small change

--- SPSS_data_process.r
+++ SPSS_data_process.r
@@ -4,12 +4,15 @@ library(foreign)
 # finding a path of a file
 datafileinput <- "data/tDCS_visualsearch_exp1.sav"
 mydata <- read.spss(datafileinput, to.data.frame = TRUE, 
-                    use.value.labels=TRUE, max.value.labels=12)
+                    use.value.labels=TRUE, max.value.labels=12, reencode=FALSE)
 
 mydata2 <- subset(mydata, select = -c(filter_., OriginalID))
 mydata3 <- subset(mydata, select = c(Stimulation))
 mydata4 <- subset(mydata, select = "Stimulation")
 mydata5 <- mydata[c("RT", "Stimulation")]
 
-head(mydata2, n=16L)
+#head(mydata2, n=16L)
+table(mydata2$Stimulation)
+
+
 
